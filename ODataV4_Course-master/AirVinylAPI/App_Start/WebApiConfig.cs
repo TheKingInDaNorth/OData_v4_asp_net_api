66896af54d0a25f8f47f6143e114d15909d3de86
TheKingInDaNorth <tuan.le.vo19@gmail.com>
2021-05-31 13:01:04 -0700

    5,31,21

---
+++ ODataV4_Course-master/AirVinylAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using AirVinyl.Model;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.OData.Edm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+
+namespace AirVinylAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            config.MapODataServiceRoute("ODataRoute", "odata", GetEdmModel());
+            config.EnsureInitialized();
+        }
+
+        public static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.Namespace = "AirVinyl";
+            builder.ContainerName = "AirVinylContainer";
+
+            builder.EntitySet<Person>("People");
+            builder.EntitySet<VinylRecord>("VinylRecords");
+
+            return builder.GetEdmModel();
+
+        }
+    }
+}
